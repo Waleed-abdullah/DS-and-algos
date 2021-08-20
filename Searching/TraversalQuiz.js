ad4dc7d0ebbee712ffd00ddeab3ee1a33bdef43a
Waleed-Abdullah <w26waleed@gmail.com>
2021-08-20 11:20:18 +0500

    traversal quiz

---
+++ Searching/TraversalQuiz.js
@@ -0,0 +1,15 @@
+//If you know a solution is not far from the root of the tree:
+breadth first search
+//If the tree is very deep and solutions are rare: 
+BFS (dfs will take long)
+//If the tree is very wide:
+depth first search (bfs will need too much memory)
+
+//If solutions are frequent but located deep in the tree:
+depth first search
+
+//Determining whether a path exists between two nodes:
+depth first search 
+
+//Finding the shortest path:
+breadth first search
