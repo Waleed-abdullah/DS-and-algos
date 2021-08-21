ecd22f34cf1a1e2ecfd575eba10deb0af6136aee
Waleed-Abdullah <w26waleed@gmail.com>
2021-08-21 11:08:33 +0500

    validated a bst

--- Searching/Validate a BST.js
+++ Searching/Validate a BST.js
@@ -17,10 +17,11 @@ var isValidBST = function (root) {
 const validate = (root, low, high) => {
   if (root === null) return true; //empty trees are valid BSTs
 
+  // The current node's value must be between low and high.
   if ((low != null && root.val <= low) || (high != null && root.val >= high)) {
     return false;
   }
-
+  // The left and right subtree must also be valid.
   return (
     validate(root.right, root.val, high) && validate(root.left, low, root.val)
   );
