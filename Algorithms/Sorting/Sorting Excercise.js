21d8d53889932a0ed47c9e6db838b8e4ac1dac56
Waleed-Abdullah <w26waleed@gmail.com>
2021-09-01 17:51:46 +0500

    sorting excercise

---
+++ Algorithms/Sorting/Sorting Excercise.js
@@ -0,0 +1,25 @@
+//#1 - Sort 10 schools around your house by distance:
+insertion sort  since the list is small we can use insertion sort we can get the best case of O(n)
+
+//#2 - eBay sorts listings by the current Bid amount:
+radix , counting sort because the numbers are alwyas going to be in a certain range
+
+//#3 - Sport scores on ESPN
+ quicksort
+
+//#4 - Massive database (can't fit all into memory) needs to sort through past year's user data
+mergesort(we will not be sorting in memory,data is very large so worst case of quicksort is bad)
+
+//#5 - Almost sorted Udemy review data needs to update and add 2 new reviews
+insertion sort
+
+
+//#6 - Temperature Records for the past 50 years in Canada
+radix counting sort if no decimal points
+quicksort
+
+//#7 - Large user name database needs to be sorted. Data is very random.
+quicksort
+
+//#8 - You want to teach sorting for the first time
+bubblesort, selection sort
